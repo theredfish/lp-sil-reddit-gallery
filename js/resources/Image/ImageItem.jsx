78dfa06fbe0d0ac6d09a15c3eb516cca6b4be98f
Julian Didier <julian.didier@etu.univ-nantes.fr>
2016-01-06 23:24:48 +0100

    Improved user interface

--- js/resources/Image/ImageItem.jsx
+++ js/resources/Image/ImageItem.jsx
@@ -1,10 +1,10 @@
 let ImageItem = React.createClass({
 	render: function() {
 		return (
-			<div className="img-box" contentEditable="true">
+			<div className="img-box" tabIndex="0">
                 <div className="img-content">
-                    <img src={this.props.url} alt={this.props.title} contentEditable="false" />
-					<div className="img-title" contentEditable="false">{this.props.title}</div>
+                    <img src={this.props.url} alt={this.props.title}/>
+					<div className="img-title">{this.props.title}</div>
                 </div>
 			</div>
 		);
